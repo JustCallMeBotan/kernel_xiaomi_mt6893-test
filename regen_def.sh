d7e8b6cd3cfe6f12eec880c6657fd1640a46f430
Botan <botan@kucinghitam.pp.ua>
2026-02-24 12:15:13 +0700

    Update regen_def.sh

--- regen_def.sh
+++ regen_def.sh
@@ -2,17 +2,17 @@
 
 if [ "$1" = "--ares" ]; then
     export DEVICE=ares
-    export DEFCONFIG=ares_user_defconfig
+    export DEFCONFIG=ares_defconfig
 elif [ "$1" = "--chopin" ]; then
     export DEVICE=chopin
-    export DEFCONFIG=chopin_user_defconfig
+    export DEFCONFIG=chopin_defconfig
 elif [ "$1" = "--agate" ]; then
      export DEVICE=agate
-     export DEFCONFIG=agate_user_defconfig 
+     export DEFCONFIG=agate_defconfig 
 else
-    export DEFCONFIG_1=chopin_user_defconfig
-    export DEFCONFIG_2=ares_user_defconfig
-    export DEFCONFIG_3=agate_user_defconfig
+    export DEFCONFIG_1=chopin_defconfig
+    export DEFCONFIG_2=ares_defconfig
+    export DEFCONFIG_3=agate_defconfig
 fi
 
 if [ -z "$1" ]; then

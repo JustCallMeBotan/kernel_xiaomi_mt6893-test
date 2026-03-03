f6c95efe49cd30d38e5db57fe5c5dbffde537d20
Botan Kurashiki <botan@kucinghitam.pp.ua>
2026-03-03 08:42:58 +0700

    change kernelname and build script

--- build.sh
+++ build.sh
@@ -1,64 +1,126 @@
 #!/bin/bash
-#
-# Compile script for Hydrogen kernel
-# Brought to you by rio004
-#
 
-# Date/Time
-SECONDS=0
-DATE=$(date '+%Y%m%d-%H%M')
+function compile()
+{
+    source ~/.bashrc && source ~/.profile
+    export LC_ALL=C && export USE_CCACHE=1
+    ccache -M 10G
+    export ARCH=arm64
+    DATE=$(date '+%Y%m%d-%H%M')
+    export KBUILD_BUILD_HOST=android-build-mtk
+    export KBUILD_BUILD_USER="BotanKurashiki"
 
-# Device
-DEVICE="${1:-agate}"
-DEFCONFIG="${DEVICE}_defconfig"
-ZIPNAME="HydrogenKernel-${DEVICE}-${DATE}.zip"
+    git clone --depth=1 https://gitlab.com/LeCmnGend/proton-clang.git -b clang-17 clang
 
-echo -e "Building for: $DEVICE\n"
-
-# Ensure the toolchain is available
-TC_DIR="$HOME/toolchains/proton-clang"
-CURRENT_DIR=$(pwd)
-if [ ! -d "$TC_DIR" ]; then
-    mkdir -p "$HOME/toolchains"
-    cd "$HOME/toolchains"
-    git clone --depth=1 https://gitlab.com/LeCmnGend/proton-clang.git -b clang-15 proton-clang
-    cd "$CURRENT_DIR"
-fi
-export PATH="$TC_DIR/bin:$PATH"
-
-# Process options
-CLEAN_BUILD=false
-INCLUDE_KSU=false
-for arg in "$@"; do
-    case $arg in
-        -c) CLEAN_BUILD=true ;;
-        -ksu) INCLUDE_KSU=true
-             ZIPNAME="HydrogenKernel-KSU-${DEVICE}-${DATE}.zip"
-             ;;
+    # Argument checks
+    case "$1" in
+        --ares)
+            export DEVICE=ares
+            export DEFCONFIG=ares_defconfig
+            ;;
+        --chopin)
+            export DEVICE=chopin
+            export DEFCONFIG=chopin_defconfig
+            ;;
+        --agate)
+            export DEVICE=agate
+            export DEFCONFIG=agate_defconfig
+            ;;
+        *)
+            echo "Usage: $0 <device> [--permissive]"
+            echo ""
+            echo "Available <device> options:"
+            echo "  --ares       Ares"
+            echo "  --chopin     Chopin"
+            echo "  --agate      Agate"
+            echo ""
+            echo "Optional flags:"
+            echo "  --permissive     Make kernel SELinux permissive"
+            exit 1
+            ;;
     esac
-done
 
-[ "$CLEAN_BUILD" = true ] && rm -rf out
-[ "$INCLUDE_KSU" = true ] && echo "Save your stuff!!" && curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s v0.9.5
-
-# Compilation process
-mkdir -p out
-make O=out ARCH=arm64 $DEFCONFIG
-
-echo -e "\nStarting compilation...\n"
-if make -j$(nproc --all) O=out ARCH=arm64 CC="ccache clang" LLVM=1 LLVM_IAS=1 CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz; then
-    echo -e "\nKernel compiled successfully! Zipping up...\n"
-    git clone -q --depth=1 https://github.com/rio004/AnyKernel3 AnyKernel3
-    cp out/arch/arm64/boot/Image.gz AnyKernel3
-    rm -rf *zip out/arch/arm64/boot
-    (cd AnyKernel3 && zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder)
-    rm -rf AnyKernel3
-    if [ "$INCLUDE_KSU" = true ]; then
-        git restore drivers/{Makefile,Kconfig}
-        rm -rf KernelSU drivers/kernelsu
+    # Permissive mode check (second argument)
+    if [ "$2" = "--permissive" ]; then
+        export KERNEL_PERMISSIVE=true
+    else
+        export KERNEL_PERMISSIVE=false
     fi
-    echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)!"
-    echo "Zip: $ZIPNAME"
-else
-    echo -e "\nCompilation failed!"
-fi
+
+    if ! [ -d "out" ]; then
+        echo "Kernel OUT Directory Not Found. Making Again"
+        mkdir out
+    fi
+
+    make O=out ARCH=arm64 $DEFCONFIG
+
+    if [ "$KERNEL_PERMISSIVE" = true ]; then
+        current_cmdline=$(grep '^CONFIG_CMDLINE=' out/.config | cut -d= -f2- | sed 's/^"//' | sed 's/"$//')
+        if [[ "$current_cmdline" != *"androidboot.selinux=permissive"* ]]; then
+            new_cmdline="${current_cmdline} androidboot.selinux=permissive"
+            scripts/config --file out/.config \
+                --set-str CONFIG_CMDLINE "$new_cmdline"
+    # Append cmdline to bootloader cmdline instead of replacing
+    scripts/config --file out/.config \
+        --enable CONFIG_CMDLINE_EXTEND \
+        --disable CONFIG_CMDLINE_FORCE
+
+# Regenerate config with new settings
+    make O=out ARCH=arm64 olddefconfig
+        fi
+    fi
+
+    mkdir tmp
+    cp -r out/.config tmp/final_config
+    find out -type f -name "*.ko" -delete
+    make O=out ARCH=arm64 tmp/final_config
+    rm -rf tmp 
+
+    CCACHE_EXEC=$(which ccache)
+
+    PATH="${PWD}/clang/bin:${PATH}" \
+    make -j$(nproc --all) O=out \
+        ARCH=arm64 \
+        CC="ccache clang" \
+        CLANG_TRIPLE=aarch64-linux-gnu- \
+        CROSS_COMPILE="${PWD}/clang/bin/aarch64-linux-gnu-" \
+        CROSS_COMPILE_ARM32="${PWD}/clang/bin/arm-linux-gnueabi-" \
+        LD=ld.lld \
+        STRIP=llvm-strip \
+        AS=llvm-as \
+        AR=llvm-ar \
+        NM=llvm-nm \
+        OBJCOPY=llvm-objcopy \
+        OBJDUMP=llvm-objdump \
+	modules \
+	Image.gz-dtb modules \
+        CONFIG_NO_ERROR_ON_MISMATCH=y 2>&1 | tee error.log
+}
+
+function zupload()
+{
+    rm -rf AnyKernel
+    git clone --depth=1 https://github.com/JustCallMeBotan/AnyKernel33 -b $DEVICE AnyKernel
+    mkdir -p AnyKernel/modules/system/vendor/lib/modules
+    find out -type f -name "*.ko" -exec cp -f {} AnyKernel/modules/system/vendor/lib/modules \;
+    
+    if [ "$DEVICE" = "agate" ]; then
+        cp out/arch/arm64/boot/Image.gz AnyKernel
+    else
+        cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+    fi
+
+    if [ "$KERNEL_PERMISSIVE" = true ]; then
+        SELINUX_MODE="Permissive"
+    else
+        SELINUX_MODE="Enforcing"
+    fi
+
+    cd AnyKernel
+    zip -r9 4.14.336-Test-OSS-KERNEL-${DEVICE}-${SELINUX_MODE}-${DATE}-SUkiSU.zip * -x '*.git*' README.md *placeholder
+    cd ../
+}
+
+# Call compile with args
+compile "$1" "$2"
+zupload
